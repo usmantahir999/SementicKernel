2571d8a78d16460337556853124805f87fa45859
Usman Tahir <Usman.tahir@synergy-it.com>
2025-07-27 15:24:25 +0500

    45. Build the Chat Popup View - 2

---
+++ Chatbot/Controllers/ChatController.cs
@@ -0,0 +1,29 @@
+﻿using Chatbot.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+
+namespace Chatbot.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatController(SemanticKernelService semanticKernelService) : ControllerBase
+    {
+        private readonly SemanticKernelService _semanticKernelService = semanticKernelService;
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            return Ok("Welcome to the Chatbot API. Use the /GetChatResponseAsync endpoint to interact with the chatbot.");
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> GetChatResponseAsync(ChatRequest request)
+        {
+           var result = await _semanticKernelService.GetChatResponseAsync(request.UserMessage);
+            return Ok(result);
+        }
+    }
+
+    public record ChatRequest(string UserMessage);
+}
---
+++ Chatbot/Services/SemanticKernelService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.SemanticKernel;
+
+namespace Chatbot.Services
+{
+    public class SemanticKernelService
+    {
+        private readonly Kernel _kernel;
+        public SemanticKernelService(IConfiguration configuration)
+        {
+            string apiKey = configuration["SementicKernel:ApiKey"];
+            string endpoint = configuration["SementicKernel:Endpoint"];
+            string deploymentName = configuration["SementicKernel:DeploymentName"];
+            var builder = Kernel.CreateBuilder();
+
+            builder.AddAzureOpenAIChatCompletion(
+           deploymentName: deploymentName,
+           endpoint: endpoint,
+           apiKey: apiKey);
+
+            _kernel = builder.Build();
+        }
+
+        public async Task<string> GetChatResponseAsync(string prompt)
+        {
+            var response = await _kernel.InvokePromptAsync(prompt);
+            Console.WriteLine($"Response: {response}");
+            return response.ToString();
+        }
+    }
+}
